efd0f0dd4e807160018106addea379ff597c7566
dominicho97 <dominic.ho97@gmail.com>
2019-03-14 14:11:28 +0100

    added mysqli_connect and error message

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,4 +4,10 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "";
+$dBName = "loginsystem";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName );
+
+if(!$conn) {
+  die("Connection failed: ".mysqli_connect_error());
+}
