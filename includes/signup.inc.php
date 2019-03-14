a284659822c9174a8cd99f7acf0d31b1d1e06d32
dominicho97 <dominic.ho97@gmail.com>
2019-03-14 14:52:43 +0100

    added code to check email

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,10 +7,18 @@ require 'dbh.inc.php';
 
 $username = $_POST['uid'];
 $email = $_POST['mail'];
-$pwd = $_POST['pwd'];
+$password = $_POST['pwd'];
 $passwordRepeat = $_POST['pwd-repeat'];
 
 
+ if (empty($username)|| empty($email) || empty($password) || empty($passwordRepeat)){
+  header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
+  exit();
+ }
+ else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  header("Location: ../signup.php?error=invalidmail&uid=".$username);
+  exit();
+ }
 
 
 
