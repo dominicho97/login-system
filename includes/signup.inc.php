93c8405185f9c41f27ee5ceffc2e66422aabf4b2
dominicho97 <dominic.ho97@gmail.com>
2019-03-14 14:15:01 +0100

    connected  on signup.inc

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -2,6 +2,17 @@
 
 if(isset($_POST['signup-submit'])) {
 
+require 'dbh.inc.php';
 
-  
+
+$username = $_POST['uid'];
+$email = $_POST['mail'];
+$pwd = $_POST['pwd'];
+$passwordRepeat = $_POST['pwd-repeat'];
+
+
+
+
+
+
 }
