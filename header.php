0f37bce154d2d5d2e7b8ff8de1926410902c81d7
dominicho97 <dominic.ho97@gmail.com>
2019-03-14 10:34:11 +0100

    clean up code with indents

--- header.php
+++ header.php
@@ -9,30 +9,29 @@
 <body>
   
 <header>
-<nav>
-<a href = "#">
-  <img src="img/lildomsq.PNG" alt="logo">
+  <nav>
+    <a href = "#">
+      <img src="img/lildomsq.PNG" alt="logo">
+    </a>
+    <ul>
+      <li><a href="#">Home</li>
+      <li><a href="#">Portfolio</li>
+      <li><a href="#">About me</li>
+      <li><a href="#">Contact</li>
+    </ul>
+  <div>
 
-</a>
-<ul>
-  <li><a href="#">Home</li>
-  <li><a href="#">Portfolio</li>
-  <li><a href="#">About me</li>
-  <li><a href="#">Contact</li>
-</ul>
-<div>
+    <form action="includes/login.inc.php" method="post">
+      <input type="text" name="mailuid" placeholder="Username/E-mail...">
+      <input type="password" name="pwd" placeholder="Password...">
+      <button type="submit" name="login-submit">Login</button>
+    </form>
 
-<form action="includes/login.inc.php" method="post">
-  <input type="text" name="mailuid" placeholder="Username/E-mail...">
-  <input type="password" name="pwd" placeholder="Password...">
-  <button type="submit" name="login-submit">Login</button>
-</form>
-
-<a href="signup.php">Signup</a>
-<form action="includes/logout.inc.php" method="post">
-  <button type="submit" name="logout-submit">Logout</button>
-</form>
-</div>
-</nav>
+    <a href="signup.php">Signup</a>
+    <form action="includes/logout.inc.php" method="post">
+      <button type="submit" name="logout-submit">Logout</button>
+    </form>
+    </div>
+  </nav>
 
 </header>
